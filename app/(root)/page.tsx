d4f35a0b9d9484a86db530f48dc950a2e34a4359
ekambains <bainseka@sheridancollege.ca>
2025-03-05 20:18:29 -0500

    Intro component complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-max max-w-screen">
-      <div className="max-w-[600px] sm:max-w-2xl sm:px-8 sm:py-8 px-7 py-7">
+      <div className="max-w-screen mx-6 sm:max-w-2xl sm:px-8 sm:py-8 px-7 py-7">
         <Navbar />
         <Intro />
       </div>
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import Image from 'next/image'
+import Link from 'next/link'
 
 const Intro = () => {
   return (
     <div className='flex flex-col items-center sm:flex-row py-8 mt-[50px] gap-6'>
         <div>
-            <img src={`/vercel.svg`} alt='Profile Picture' height={100} width={50} className='rounded-full' />
+            <img src={`/vercel.svg`} alt='Profile Picture' height={150} width={150} className='rounded-full' />
         </div>
-        <div className='flex flex-col items-center justify-center bg-red-500'>
-          <div className='text-center'>
-            <p className='text-3xl font-bold'>Hey, I am Ekampreet. I am a Software Engineer.</p>
-          </div>
+        <div className='flex flex-col text-gray-400'>
           <div>
-            
+            <p className='text-2xl font-bold'><span className='text-white'>Hey, I'm Ekampreet.</span> I am a Software Engineer and a Freelancer.</p>
+          </div>
+          <div className='flex sm:flex-row flex-col mt-6 gap-4 text-xl'>
+            <Link href={`https://www.twitter.com/ekambains`}>Twitter</Link>
+            <Link href={`https://www.linkedin.com/ekambains`}>LinkedIn</Link>
+            <Link href={`https://www.github.com/ekambains`}>Github</Link>
           </div>
         </div>
     </div>
