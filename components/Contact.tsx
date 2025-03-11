78a39555dc8e0d56b9b2c41c4fe4843d5d055be0
ekambains <bainseka@sheridancollege.ca>
2025-03-10 23:41:03 -0400

    Contact somewhat done.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-        
+    <div className='bg-white/10 backdrop-blur-lg border border-white/20 rounded-lg p-6 shadow-lg max-w-md mx-auto my-10'>
+        <form className='flex flex-col items-center' action="">
+            <h1 className='text-white text-3xl font-bold'>Send Me a Message 🚀</h1>
+            
+        </form>
     </div>
   )
 }
