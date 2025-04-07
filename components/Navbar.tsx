7217ec484f96f40f4af80ce249b7201ae4454f7a
ekambains <bainseka@sheridancollege.ca>
2025-04-07 01:25:48 -0400

    Overlay Changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,37 @@
-"use client";
+'use client';
 import React, { useState } from 'react'
 import Link from 'next/link'
-import Overlay from './Overlay';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import Overlay from './Overlay'
+import { Bars3Icon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
-    const [overlay, setOverlay] = useState(false);
-    const sidebar = () => {
-        setOverlay(!overlay);
-    };
+  const [overlay, setOverlay] = useState(false)
+  const toggleOverlay = () => setOverlay(!overlay)
+
   return (
     <>
-    <div className='flex justify-between text-gray-400 min-w-[350px] sm:min-w-2xl'>
+      <div className='flex justify-between text-gray-400 min-w-[350px] sm:min-w-2xl'>
         <div>
-            <Link href={`/`}><span className='font-semibold text-xl hover:cursor-pointer tracking-widest hover:text-blue-500'>BAINS</span></Link>
+          <Link href={`/`}>
+            <span className='font-semibold text-xl hover:cursor-pointer tracking-widest hover:text-blue-500'>BAINS</span>
+          </Link>
         </div>
         <div className='sm:flex sm:justify-between sm:items-center gap-8 hidden'>
-            <Link href={`/about`}>
-                <span className='hover:text-blue-500'>About</span>
-            </Link>
-            <Link href={`/contact`}>
-                <span className='hover:text-blue-500'>Contact</span>
-            </Link>
+          <Link href={`/about`}>
+            <span className='hover:text-blue-500'>About</span>
+          </Link>
+          <Link href={`/contact`}>
+            <span className='hover:text-blue-500'>Contact</span>
+          </Link>
         </div>
         <div className='sm:hidden'>
-            <button onClick={sidebar}>
-                {overlay ? (<XMarkIcon className='size-8' />) : (<Bars3Icon className='size-8' />)}
-            </button>
+          <button onClick={toggleOverlay}>
+            <Bars3Icon className='size-8' />
+          </button>
         </div>
-    </div>
-    {overlay && (<Overlay />)}
+      </div>
+
+      {overlay && <Overlay close={() => setOverlay(false)} />}
     </>
   )
 }
--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
+import { XMarkIcon } from '@heroicons/react/24/solid'
 
-const Overlay = () => {
+const Overlay = ({ close }: { close: () => void }) => {
   return (
-    <div className='flex flex-col h-screen z-50 px-8 py-5 gap-4 text-xl text-white sm:hidden'>
+    <div className='fixed inset-0 bg-black z-50 flex flex-col px-10 py-7 gap-4 text-xl text-white sm:hidden'>
+      <div className='flex justify-end'>
+        <button onClick={close}>
+          <XMarkIcon className='size-8' />
+        </button>
+      </div>
       <Link href={`/`}>
-        <span>Home</span>
+        <span onClick={close}>Home</span>
       </Link>
       <Link href={`/about`}>
-        <span>About</span>
+        <span onClick={close}>About</span>
       </Link>
       <Link href={`/contact`}>
-        <span>Contact</span>
+        <span onClick={close}>Contact</span>
       </Link>
     </div>
   )
